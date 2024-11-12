f4bd88d76bad34d38dfa0bfbae34ea5f55819645
Kevin FERNANDO <kevin.fernando@supinfo.com>
2024-11-12 14:35:40 +0100

    Chapter 9: Streaming
    wrapped <Card /> component in a suspense component and moved its data fetcher inside it

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,5 +1,6 @@
 import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
 
 export type CardProps = {
   title: string;
@@ -15,18 +16,17 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } =
+    await fetchCardData();
+
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" />
     </>
   );
 }
