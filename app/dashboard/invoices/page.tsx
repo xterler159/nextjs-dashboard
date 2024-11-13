7d3e4e70e74f4b132dedad2120d80b8e54fbc92e
Kevin FERNANDO <kevin.fernando@supinfo.com>
2024-11-13 09:59:43 +0100

    Chapter 11: Adding search and pagination
    working on invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,30 @@
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import { Suspense } from "react";
+import { InvoiceSkeleton } from "@/app/ui/skeletons";
+import Table from "@/app/ui/invoices/table";
+
 const Invoices = () => {
-  return <p>Invoices Page</p>;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+
+      {/*<Suspense fallback={<InvoiceSkeleton />}>*/}
+      {/*  <Table />*/}
+      {/*</Suspense>*/}
+
+      <div className="mt-5 flex w-full justify-center">{/*<Pagination totalPages={20} />*/}</div>
+    </div>
+  );
 };
 
 export default Invoices;
